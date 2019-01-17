42bd713685523df435b53f6c91d704817c0d1233
Simon <simon.j.goring@gmail.com>
2019-01-17 13:38:20 -0800

    Modifying the search query to return more nodes and limit the end type for the annotation.

--- helpers/searchAnnotations/searchNodes.js
+++ helpers/searchAnnotations/searchNodes.js
@@ -19,7 +19,7 @@ function searchNodes(req, res) {
 
   const session = driver.session();
 
-  session.run(cypher, {search: req.query.search, limit: 10})
+  session.run(cypher, {search: req.query.search, limit: 30})
   .then(result => {
     res.status(200)
       .json({
