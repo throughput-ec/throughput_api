4f0a03cbfc44377812b00ce2b60093aebdd8a436
Simon <simon.j.goring@gmail.com>
2020-03-04 15:00:03 -0800

    Adding the webhooks.

--- app.js
+++ app.js
@@ -60,6 +60,25 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.post("/webhooks/github", function (req, res) {
+  var sender = req.body.sender;
+  var branch = req.body.ref;
+
+  if(branch.indexOf('master') > -1 && sender.login === 'throughput-ec'){
+      deploy(res);
+  }
+})
+
+function deploy(res){
+  childProcess.exec('cd /home && ./deploy.sh', function(err, stdout, stderr){
+      if (err) {
+       console.error(err);
+       return res.send(500);
+      }
+      res.send(200);
+    });
+}
+
 app.set('port', process.env.PORT || 3000)
 
 var server = app.listen(app.get('port'), function () {
