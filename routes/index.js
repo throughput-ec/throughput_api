28991c6b2494c1c5429372130dcb6fd1c5151898
Simon <simon.j.goring@gmail.com>
2020-03-17 13:56:23 -0700

    Moved api calls to the api path. This should then allow us to avoid conflicts between a Vue app and the API.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res)
+router.get('/api/', function (req, res)
 {
     res.render('index',
     {
@@ -10,46 +10,46 @@ router.get('/', function (req, res)
     });
 });
 
-router.get('/annotations', function (req, res)
+router.get('/api/annotations', function (req, res)
 {
     var annotation = require('./../helpers/postannotation/postannotation.js');
     annotation.postannotation(req, res);
 })
 
-router.get('/ccdr', function (req, res)
+router.get('/api/ccdr', function (req, res)
 {
     var ccdr = require('./../helpers/ccdr/ccdr.js');
     console.log(req.query)
     ccdr.searchCcdrs(req, res);
 })
 
-router.get('/metrics/type', function (req, res)
+router.get('/api/metrics/type', function (req, res)
 {
     var metrics = require('./../helpers/metrics/metrics.js');
     metrics.nodesByType(req, res);
 })
 
-router.get('/metrics/annos', function (req, res)
+router.get('/api/metrics/annos', function (req, res)
 {
     var metrics = require('./../helpers/metrics/metrics.js');
     metrics.totalAnnot(req, res);
 })
 
 
-router.get('/repo', function (req, res)
+router.get('/api/repo', function (req, res)
 {
     var ccdr = require('./../helpers/searchRepo/coderepo.js');
     console.log(req.query)
     ccdr.searchRepo(req, res);
 })
 
-router.get('/keyword', function (req, res)
+router.get('/api/keyword', function (req, res)
 {
     var query = require('./../helpers/keywords/keywords.js');
     query.keywords(req, res);
 })
 
-router.post('/datanote', function (req, res)
+router.post('/api/datanote', function (req, res)
 {
     var notes = require('./../helpers/postannotation/datanote.js');
     notes.datanote(req, res);
