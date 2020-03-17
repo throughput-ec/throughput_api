1739625f742c1845254bfb86c990596772386052
Simon <simon.j.goring@gmail.com>
2020-03-17 13:02:36 -0700

    Prettifying the app.js code.

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ app.set('view engine', 'jade');
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded(
+{
+    extended: true
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,7 +33,8 @@ app.use('/', routes);
 app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next)
+{
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -40,10 +44,13 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+if (app.get('env') === 'development')
+{
+    app.use(function (err, req, res, next)
+    {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render('error',
+        {
             message: err.message,
             error: err
         });
@@ -52,35 +59,44 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next)
+{
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('error',
+    {
         message: err.message,
-        error: {}
+        error:
+        {}
     });
 });
 
-app.post("/webhooks/github", function (req, res) {
-  var sender = req.body.sender;
-  var branch = req.body.ref;
+app.post("/webhooks/github", function (req, res)
+{
+    var sender = req.body.sender;
+    var branch = req.body.ref;
 
-  if(branch.indexOf('master') > -1 && organization.login === 'throughput-ec'){
-      deploy(res);
-  }
+    if (branch.indexOf('master') > -1 && organization.login === 'throughput-ec')
+    {
+        deploy(res);
+    }
 })
 
-function deploy(res){
-  childProcess.exec('cd /home && ./deploy.sh', function(err, stdout, stderr){
-      if (err) {
-       console.error(err);
-       return res.send(500);
-      }
-      res.send(200);
+function deploy(res)
+{
+    childProcess.exec('cd /home && ./deploy.sh', function (err, stdout, stderr)
+    {
+        if (err)
+        {
+            console.error(err);
+            return res.send(500);
+        }
+        res.send(200);
     });
 }
 
 app.set('port', process.env.PORT || 3000)
 
-var server = app.listen(app.get('port'), function () {
-  debug('Express server listening on port ' + server.address().port)
+var server = app.listen(app.get('port'), function ()
+{
+    debug('Express server listening on port ' + server.address().port)
 })
