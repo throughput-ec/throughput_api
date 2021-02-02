6108de8aa76b96737a0dfefb48978bae1d0101c7
Simon Goring <simon.j.goring@gmail.com>
2021-02-02 10:51:19 -0800

    Cleaning up the keyword searches.  Restructuring index. Fixing the grammar and hierarchical structure of the calls.  Now the last term is the thing that is being returned.

---
+++ helpers/keywords/keywordrepo.js
@@ -0,0 +1,71 @@
+const neo4j = require('neo4j-driver');
+var fs = require('fs');
+const path = require('path');
+
+var pwbin = require('./../../pwbin.json')
+
+function parsedata(records) {
+  // Takes in the fields from neo4j and turns them into a named object
+  // Otherwise the keys and field values are split into different objects.
+  var test = records.map(x => {
+    var out = {}
+    for (i = 0; i < x.keys.length; i++) {
+      out[x.keys[i]] = x._fields[i];
+    }
+    return out;
+  })
+
+  return test;
+}
+
+// Create Driver
+const driver = new neo4j.driver(pwbin.host,
+  neo4j.auth.basic(pwbin.user, pwbin.password), {
+    disableLosslessIntegers: true
+  });
+
+function keywordrepo(req, res) {
+
+  const fullPath = path.join(__dirname, 'cql/keywordRepoArray.cql');
+  var textByLine = fs.readFileSync(fullPath).toString()
+
+  params = {
+    'keywords': [''],
+    'offset': 0,
+    'limit': 25
+  }
+
+  Object.keys(params).map(x => {
+    if (!!req.query[x]) {
+      params[x] = req.query[x]
+    } else {
+      params[x] = params[x]
+    }
+  })
+
+  if (!params.keywords === ['']) {
+    params.keywords = params.keywords.split(',')
+  }
+
+  const session = driver.session();
+
+  const aa = session.readTransaction(tx => tx.run(textByLine, params))
+    .then(result => {
+      output = parsedata(result.records)
+      res.status(200)
+        .json({
+          status: 'success',
+          data: {
+            params: params,
+            data: output
+          },
+          message: 'Returning Throughput keywords.'
+        })
+    })
+    .catch(function(err) {
+      console.error(err);
+    })
+    .finally(() => session.close())
+}
+
+module.exports.keywordrepo = keywordrepo;
